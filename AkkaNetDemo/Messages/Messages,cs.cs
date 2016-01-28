8e13d40a225503faeac39d72489cd1810e5f159c
Bob Reselman <reselbob@gmail.com>
2016-01-27 23:38:08 -0800

    Added Solution Files

---
+++ AkkaNetDemo/Messages/Messages,cs.cs
@@ -0,0 +1,16 @@
+﻿namespace AkkaNetDemo.Messages
+{
+    public class Trade
+    {
+        public enum TradeType{Buy, Sell}
+        public Trade(string ticker, int shares, TradeType tradeType)
+        {
+            Ticker = ticker;
+            Shares = shares;
+            Type = tradeType;
+        }
+        public string Ticker { get; private set; }
+        public int Shares { get; private set; }
+        public TradeType Type { get; private set; }
+       }
+    }
---
+++ AkkaNetDemoTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AkkaNetDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AkkaNetDemoTest
+{
+    [TestClass]
+    public class AkkaNetTests
+    {
+        [TestMethod]
+        public void BuyTest()
+        {
+            var house = new BrokerageHouse();
+            house.Buy("IBM", 125);
+        }
+    }
+}
